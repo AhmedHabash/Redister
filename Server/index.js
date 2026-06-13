4d3fb68fd74d05cde21ad780cc36bd4178944e00
Ahmed Essam <ae2892962@gmail.com>
2026-06-13 20:25:54 +0300

    fixing .env config

--- Server/index.js
+++ Server/index.js
@@ -1,17 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-mongoose
-  .connect("mongodb://localhost:27017/employees")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("DB is good"))
   .catch((err) => console.log(err));
 
-
 app.use("/register", require("./routes/user.route"));
 app.use("/login",require("./routes/user.route"))
-app.listen(3001, () => {
-  console.log("server is lissten in port 3001");
-});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
